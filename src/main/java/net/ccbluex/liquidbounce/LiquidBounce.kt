60383417f93abca5f6852bd69bf5e6a52ac80d8f
idlevi29 <vanhao294.ts@gmail.com>
2025-07-17 17:43:40 +0700

    test

---
+++ src/main/java/net/ccbluex/liquidbounce/LiquidBounce.kt
@@ -0,0 +1,290 @@
+/*
+ * RinBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/rattermc/rinbounce69
+ */
+package net.ccbluex.liquidbounce
+
+import com.formdev.flatlaf.themes.FlatMacLightLaf
+import kotlinx.coroutines.launch
+import net.ccbluex.liquidbounce.api.ClientUpdate
+import net.ccbluex.liquidbounce.api.ClientUpdate.gitInfo
+import net.ccbluex.liquidbounce.api.loadSettings
+import net.ccbluex.liquidbounce.cape.CapeService
+import net.ccbluex.liquidbounce.event.ClientShutdownEvent
+import net.ccbluex.liquidbounce.event.EventManager
+import net.ccbluex.liquidbounce.event.StartupEvent
+import net.ccbluex.liquidbounce.features.command.CommandManager
+import net.ccbluex.liquidbounce.features.command.CommandManager.registerCommands
+import net.ccbluex.liquidbounce.features.module.ModuleManager
+import net.ccbluex.liquidbounce.features.module.ModuleManager.registerModules
+import net.ccbluex.liquidbounce.features.special.BungeeCordSpoof
+import net.ccbluex.liquidbounce.features.special.ClientFixes
+import net.ccbluex.liquidbounce.features.special.ClientRichPresence
+import net.ccbluex.liquidbounce.features.special.ClientRichPresence.showRPCValue
+import net.ccbluex.liquidbounce.file.FileManager
+import net.ccbluex.liquidbounce.file.FileManager.loadAllConfigs
+import net.ccbluex.liquidbounce.file.FileManager.saveAllConfigs
+import net.ccbluex.liquidbounce.file.configs.models.ClientConfiguration.updateClientWindow
+import net.ccbluex.liquidbounce.lang.LanguageManager.loadLanguages
+import net.ccbluex.liquidbounce.script.ScriptManager
+import net.ccbluex.liquidbounce.script.ScriptManager.enableScripts
+import net.ccbluex.liquidbounce.script.ScriptManager.loadScripts
+import net.ccbluex.liquidbounce.script.remapper.Remapper
+import net.ccbluex.liquidbounce.script.remapper.Remapper.loadSrg
+import net.ccbluex.liquidbounce.tabs.BlocksTab
+import net.ccbluex.liquidbounce.tabs.ExploitsTab
+import net.ccbluex.liquidbounce.tabs.HeadsTab
+import net.ccbluex.liquidbounce.ui.client.altmanager.GuiAltManager.Companion.loadActiveGenerators
+import net.ccbluex.liquidbounce.ui.client.clickgui.ClickGui
+import net.ccbluex.liquidbounce.ui.client.hud.HUD
+import net.ccbluex.liquidbounce.ui.font.Fonts
+import net.ccbluex.liquidbounce.utils.client.BlinkUtils
+import net.ccbluex.liquidbounce.utils.client.ClassUtils.hasForge
+import net.ccbluex.liquidbounce.utils.client.ClientUtils.LOGGER
+import net.ccbluex.liquidbounce.utils.client.ClientUtils.disableFastRender
+import net.ccbluex.liquidbounce.utils.client.PacketUtils
+import net.ccbluex.liquidbounce.utils.inventory.InventoryManager
+import net.ccbluex.liquidbounce.utils.inventory.InventoryUtils
+import net.ccbluex.liquidbounce.utils.inventory.SilentHotbar
+import net.ccbluex.liquidbounce.utils.io.MiscUtils
+import net.ccbluex.liquidbounce.utils.io.MiscUtils.showErrorPopup
+import net.ccbluex.liquidbounce.utils.kotlin.SharedScopes
+import net.ccbluex.liquidbounce.utils.movement.BPSUtils
+import net.ccbluex.liquidbounce.utils.movement.MovementUtils
+import net.ccbluex.liquidbounce.utils.movement.TimerBalanceUtils
+import net.ccbluex.liquidbounce.utils.render.MiniMapRegister
+import net.ccbluex.liquidbounce.utils.render.shader.Background
+import net.ccbluex.liquidbounce.utils.rotation.RotationUtils
+import net.ccbluex.liquidbounce.utils.timing.TickedActions
+import net.ccbluex.liquidbounce.utils.timing.WaitTickUtils
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Future
+import javax.swing.UIManager
+
+object LiquidBounce {
+
+    /**
+     * Client Information
+     *
+     * This has all the basic information.
+     */
+    const val CLIENT_NAME = "LiquidBounce"
+
+    const val CLIENT_AUTHOR = "CCBlueX, rattermc"
+    const val CLIENT_CLOUD = "https://cloud.liquidbounce.net/LiquidBounce"
+    const val CLIENT_WEBSITE = "liquidbounce.net"
+    const val CLIENT_GITHUB = "https://github.com/VuGiaBao1337/rinbounce69/"
+
+    const val MINECRAFT_VERSION = "1.8.9"
+    
+    val clientVersionText = gitInfo["git.build.version"]?.toString() ?: "fuck skidder"
+    val clientVersionNumber = clientVersionText.substring(1).toIntOrNull() ?: 0 // version format: "b<VERSION>" on legacy
+    val clientCommit = gitInfo["git.commit.id.abbrev"]?.let { "git-$it" } ?: "unknown"
+    val clientBranch = gitInfo["git.branch"]?.toString() ?: "unknown"
+
+    /**
+     * Defines if the client is in development mode.
+     * This will enable update checking on commit time instead of regular legacy versioning.
+     */
+    const val IN_DEV = false
+
+    val clientTitle = CLIENT_NAME +  clientVersionText + " ( Main ) | " + MINECRAFT_VERSION + if (IN_DEV) " | DEVELOPMENT BUILD" else ""
+
+    var isStarting = true
+
+    // Managers
+    val moduleManager = ModuleManager
+    val commandManager = CommandManager
+    val eventManager = EventManager
+    val fileManager = FileManager
+    val scriptManager = ScriptManager
+
+    // HUD & ClickGUI
+    val hud = HUD
+
+    val clickGui = ClickGui
+
+    // Menu Background
+    var background: Background? = null
+
+    // Discord RPC
+    val clientRichPresence = ClientRichPresence
+
+    /**
+     * Start IO tasks
+     */
+    fun preload(): Future<*> {
+
+        net.ccbluex.liquidbounce.utils.client.javaVersion
+
+        // Change theme of Swing
+        UIManager.setLookAndFeel(FlatMacLightLaf())
+
+        val future = CompletableFuture<Unit>()
+
+        SharedScopes.IO.launch {
+            try {
+                LOGGER.info("Starting preload tasks of $CLIENT_NAME")
+
+                // Download and extract fonts
+                Fonts.downloadFonts()
+
+                // Check update
+                ClientUpdate.reloadNewestVersion()
+
+                // Load languages
+                loadLanguages()
+
+                // Load alt generators
+                loadActiveGenerators()
+
+                // Load SRG file
+                loadSrg()
+
+                LOGGER.info("Preload tasks of $CLIENT_NAME are completed!")
+
+                future.complete(Unit)
+            } catch (e: Exception) {
+                future.completeExceptionally(e)
+            }
+        }
+
+        return future
+    }
+
+    /**
+     * Execute if client will be started
+     */
+    fun startClient() {
+        isStarting = true
+
+        LOGGER.info("Starting $CLIENT_NAME $clientVersionText $clientCommit, by $CLIENT_AUTHOR")
+
+        try {
+            // Load client fonts
+            Fonts.loadFonts()
+
+            // Register listeners
+            RotationUtils
+            ClientFixes
+            BungeeCordSpoof
+            CapeService
+            InventoryUtils
+            InventoryManager
+            MiniMapRegister
+            TickedActions
+            MovementUtils
+            PacketUtils
+            TimerBalanceUtils
+            BPSUtils
+            WaitTickUtils
+            SilentHotbar
+            BlinkUtils
+
+            // Load settings
+            loadSettings(false) {
+                LOGGER.info("Successfully loaded ${it.size} settings.")
+            }
+
+            // Register commands
+            registerCommands()
+
+            // Setup module manager and register modules
+            registerModules()
+
+            runCatching {
+                // Remapper
+                loadSrg()
+
+                if (!Remapper.mappingsLoaded) {
+                    error("Failed to load SRG mappings.")
+                }
+
+                // ScriptManager
+                loadScripts()
+                enableScripts()
+            }.onFailure {
+                LOGGER.error("Failed to load scripts.", it)
+            }
+
+            // Load configs
+            loadAllConfigs()
+
+            // Update client window
+            updateClientWindow()
+
+            // Tabs (Only for Forge!)
+            if (hasForge()) {
+                BlocksTab()
+                ExploitsTab()
+                HeadsTab()
+            }
+
+            // Disable Optifine FastRender
+            disableFastRender()
+
+            // Setup Discord RPC
+            if (showRPCValue) {
+                SharedScopes.IO.launch {
+                    try {
+                        clientRichPresence.setup()
+                    } catch (throwable: Throwable) {
+                        LOGGER.error("Failed to setup Discord RPC.", throwable)
+                    }
+                }
+            }
+
+            // Login into known token if not empty
+            if (CapeService.knownToken.isNotBlank()) {
+                SharedScopes.IO.launch {
+                    runCatching {
+                        CapeService.login(CapeService.knownToken)
+                    }.onFailure {
+                        LOGGER.error("Failed to login into known cape token.", it)
+                    }.onSuccess {
+                        LOGGER.info("Successfully logged in into known cape token.")
+                    }
+                }
+            }
+
+            // Refresh cape service
+            CapeService.refreshCapeCarriers {
+                LOGGER.info("Successfully loaded ${it.size} cape carriers.")
+            }
+
+            // Load background
+            FileManager.loadBackground()
+        } catch (e: Exception) {
+            LOGGER.error("Failed to start client: ${e.message}")
+            e.showErrorPopup()
+        } finally {
+            // Set is starting status
+            isStarting = false
+
+            if (!FileManager.firstStart && FileManager.backedup) {
+                SharedScopes.IO.launch {
+                    MiscUtils.showMessageDialog("Warning: backup triggered", "Client update detected! Please check the config folder.")
+                }
+            }
+
+            EventManager.call(StartupEvent)
+            LOGGER.info("Successfully started client")
+        }
+    }
+
+    /**
+     * Execute if client will be stopped
+     */
+    fun stopClient() {
+        // Call client shutdown
+        EventManager.call(ClientShutdownEvent)
+
+        // Stop all CoroutineScopes
+        SharedScopes.stop()
+
+        // Save all available configs
+        saveAllConfigs()
+    }
+
+}
