29a5d75770760afc6125b95407493dce634929cd
idlevi29 <vanhao294.ts@gmail.com>
2025-07-23 21:38:33 +0700

    huhu

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/FakeLag.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/combat/FakeLag.kt
@@ -230,7 +230,7 @@ object  FakeLag : Module("FakeLag", Category.COMBAT, gameDetecting = false) {
                     dynLastDisableTime = now
                 } else if (!Blink.blinkingSend() && distance > dynStopRange && distance < dynStartRange) {
                     dynLastStartBlinkTime = now
-                    Blink.blinking = true
+                    Blink.startBlink()
                 } else if (Blink.blinkingSend() && distance > dynStartRange) {
                     blink()
                 } else if (distance > dynMaxTargetRange) {
