544793db06eb61d8cdd0429919b74b78aef3f55d
TuiLaZeroTwo <longvenn2009@gmail.com>
2025-07-25 13:53:18 +0700

    deded

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiNewChat.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/gui/MixinGuiNewChat.java
@@ -1,3 +1,8 @@
+/*
+ * RinBounce Hacked Client
+ * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
+ * https://github.com/rattermc/rinbounce69
+ */
 package net.ccbluex.liquidbounce.injection.forge.mixins.gui;
 
 import net.ccbluex.liquidbounce.features.module.modules.misc.ChatControl;
@@ -37,67 +42,67 @@ public abstract class MixinGuiNewChat {
     @Shadow protected abstract void setChatLine(IChatComponent p_146237_1_, int p_146237_2_, int p_146237_3_, boolean p_146237_4_);
 
 
-    @Redirect(method = {"getChatComponent", "drawChat"}, at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/FontRenderer;FONT_HEIGHT:I"))
-    private int iFC(FontRenderer i) {
-        return HUD.INSTANCE.shouldModifyChatFont() ? Fonts.fontSemibold40.getHeight() : i.FONT_HEIGHT;
+    @Redirect(method = "drawChat", at = @At(value = "FIELD", target = "Lnet/minecraft/client/gui/FontRenderer;FONT_HEIGHT:I"))
+    private int injectFontChat(FontRenderer instance) {
+        return HUD.INSTANCE.shouldModifyChatFont() ? Fonts.fontSemibold40.getHeight() : instance.FONT_HEIGHT;
     }
 
     @Redirect(method = "drawChat", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;drawStringWithShadow(Ljava/lang/String;FFI)I"))
-    private int iFCB(FontRenderer i, String t, float x, float y, int c) {
-        return HUD.INSTANCE.shouldModifyChatFont() ? Fonts.fontSemibold40.drawStringWithShadow(t, x, y, c) : i.drawStringWithShadow(t, x, y, c);
+    private int injectFontChatB(FontRenderer instance, String text, float x, float y, int color) {
+        return HUD.INSTANCE.shouldModifyChatFont() ? Fonts.fontSemibold40.drawStringWithShadow(text, x, y, color) : instance.drawStringWithShadow(text, x, y, color);
     }
 
     @Redirect(method = "getChatComponent", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/FontRenderer;getStringWidth(Ljava/lang/String;)I"))
-    private int iFCC(FontRenderer i, String t) {
-        return HUD.INSTANCE.shouldModifyChatFont() ? Fonts.fontSemibold40.getStringWidth(t) : i.getStringWidth(t);
+    private int injectFontChatC(FontRenderer instance, String text) {
+        return HUD.INSTANCE.shouldModifyChatFont() ? Fonts.fontSemibold40.getStringWidth(text) : instance.getStringWidth(text);
     }
 
     @Inject(method = "printChatMessage", at = @At("HEAD"), cancellable = true)
-    public void onP(IChatComponent cc, CallbackInfo ci) {
-        String rm = cc.getFormattedText(); 
-        String mk = rm;
+    public void onPrintChatMessage(IChatComponent chatComponent, CallbackInfo ci) {
+        String rawMessage = chatComponent.getFormattedText(); 
+        String messageKey = rawMessage;
 
         if (ChatControl.INSTANCE.handleEvents() && ChatControl.INSTANCE.getStackMessage()) {
-            int[] d = m.getOrDefault(mk, new int[]{0, -1});
-            int c = d[0] + 1;
-            int lpci = d[1];
+            int[] messageData = m.getOrDefault(messageKey, new int[]{0, -1});
+            int currentCount = messageData[0] + 1;
+            int lastChatLineID = messageData[1];
 
-            IChatComponent cpt;
+            IChatComponent componentToPrint;
             
-            if (c > 1) {
-                if (lpci != -1) {
-                    mc.ingameGUI.getChatGUI().deleteChatLine(lpci);
+            if (currentCount > 1) {
+                if (lastChatLineID != -1) {
+                    mc.ingameGUI.getChatGUI().deleteChatLine(lastChatLineID);
                 }
-                String ms = rm + " " + EnumChatFormatting.GRAY + "[" + c + "x]";
-                cpt = new ChatComponentText(ms);
+                String modifiedMessage = rawMessage + " " + EnumChatFormatting.GRAY + "[" + currentCount + "x]";
+                componentToPrint = new ChatComponentText(modifiedMessage);
             } else {
-                cpt = cc;
+                componentToPrint = chatComponent;
             }
 
             ci.cancel();
 
-            int nci = ++this.customChatLineIdCounter; 
+            int newChatLineID = ++this.customChatLineIdCounter;
             
-            this.setChatLine(cpt, nci, mc.ingameGUI.getUpdateCounter(), false); 
+            this.setChatLine(componentToPrint, newChatLineID, mc.ingameGUI.getUpdateCounter(), false); 
             
-            m.put(mk, new int[]{c, nci});
+            m.put(messageKey, new int[]{currentCount, newChatLineID});
         }
     }
 
     @Redirect(method = "setChatLine", at = @At(value = "INVOKE", target = "Ljava/util/List;size()I", ordinal = 0))
-    private int hNLL(List<ChatLine> l) {
-        final ChatControl cc = ChatControl.INSTANCE;
+    private int hookNoLengthLimit(List<ChatLine> list) {
+        final ChatControl chatControl = ChatControl.INSTANCE;
 
-        if (cc.handleEvents() && cc.getNoLengthLimit()) {
+        if (chatControl.handleEvents() && chatControl.getNoLengthLimit()) {
             return -1; 
         } 
-        return l.size(); 
+        return list.size(); 
     } 
     
     @Inject(method = "clearChatMessages", at = @At("HEAD"), cancellable = true) 
-    private void hCC(CallbackInfo ci) { 
-        final ChatControl cc = ChatControl.INSTANCE; 
-        if (cc.handleEvents() && cc.getNoChatClear()) { 
+    private void hookChatClear(CallbackInfo ci) { 
+        final ChatControl chatControl = ChatControl.INSTANCE; 
+        if (chatControl.handleEvents() && chatControl.getNoChatClear()) { 
             ci.cancel(); 
         } 
     }
