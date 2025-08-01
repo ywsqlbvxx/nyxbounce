c67fba39dd847a12ad078ef40792e300838d7bea
thamturin <ahihidiscord@gmail.com>
2025-08-01 16:05:58 +0700

    ok

---
+++ src/main/java/net/ccbluex/liquidbounce/utils/MinecraftInstance.kt
@@ -0,0 +1,11 @@
+
+package net.ccbluex.liquidbounce.utils
+
+import net.minecraft.client.Minecraft
+
+open class MinecraftInstance {
+    companion object {
+        @JvmField
+        val mc: Minecraft = Minecraft.getMinecraft()
+    }
+}
