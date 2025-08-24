6fb2b33af3c3281c99209866c4cf949bdf59c6b4
ywsqlbvxx <bejaranovieragustavo@gmail.com>
2025-08-24 15:58:59 -0400

    Update BlockDrop.kt

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flymodes/ncp/BlockDrop.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/flymodes/ncp/BlockDrop.kt
@@ -3,11 +3,11 @@
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
  * https://github.com/SkidderMC/FDPClient/
  */
-package net.ccbluex.liquidbounce.features.module.modules.movement.flys.ncp
+package net.ccbluex.liquidbounce.features.module.modules.movement.flymodess.ncp
 
 import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.UpdateEvent
-import net.ccbluex.liquidbounce.features.module.modules.movement.flys.FlyMode
+import net.ccbluex.liquidbounce.features.module.modules.movement.flymodes.FlyMode
 import net.ccbluex.liquidbounce.features.value.FloatValue
 import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.utils.PacketUtils
